763bc5d9ee844105e5b74c4acddbdbae7b4843b0
TACHA1986 <126571508+TACHA1986@users.noreply.github.com>
2023-03-12 00:03:30 +0100

    Homework
    homework Java, 
    Octo/Decimal
    Person/Student

---
+++ Person.java
@@ -0,0 +1,34 @@
+package Homework;
+
+public class Person {
+    public static void main(String[] args) {
+        Student a = new Student();
+
+        System.out.println(a.FirstName);
+        System.out.println(a.LastName);
+        System.out.println(a.age);
+
+        System.out.println();
+
+        Student b = new Student();
+
+        System.out.println(b.FirstName2);
+        System.out.println(b.LastName2);
+        System.out.println(b.age2);
+
+        System.out.println();
+
+        Student c = new Student();
+
+        System.out.println(c.FirstName3);
+        System.out.println(c.LastName3);
+        System.out.println(c.age3);
+
+
+
+
+
+
+    }
+
+}
---
+++ Student.java
@@ -0,0 +1,16 @@
+package Homework;
+
+public class Student {
+    public String LastName = "Ivanov";
+    public String LastName2 = "Petrov";
+    public String LastName3 = "Sidorov";
+    public String FirstName = "Igor";
+    public String FirstName2 = "Ivan";
+    public String FirstName3 = "Anton";
+    public int age = 20;
+    public int age2 = 23;
+    public int age3 = 21;
+
+
+
+}
